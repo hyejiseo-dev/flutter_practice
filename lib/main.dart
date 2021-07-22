d5c8439b67f39a3c0bc76e1fa52b3b8c40b3f1fd
hyejiseo-dev <dianna537@gmail.com>
2021-07-22 16:22:36 +0900

    widget practice

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,33 @@ class MyApp extends StatelessWidget {
       title: 'First app',
       theme: ThemeData(primarySwatch: Colors.blue),
       home:
-          MyHomePage(), //가장 처음 실행되는 부분!(변하는 부분이 있다면 StatefulWidget, 글자만 출력할거면 StatelessWidget)
+          MyCard(), //가장 처음 실행되는 부분!(변하는 부분이 있다면 StatefulWidget, 글자만 출력할거면 StatelessWidget)
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyCard extends StatelessWidget {
   //const ({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     //Scaffold = 발판을 만들다 라는 의미로 다양한 요소들을 배치하게 해주는 빈 도화지 기능(어떤 요소를 배치하기 위함)
     return Scaffold(
-      appBar: AppBar(
-        title: Text('First App'),
-      ),
-      body: Center(
-        //모든 요소를 가운데 정렬
-        child: Column(
-          //모든 요소를 세로로 배치
-          children: <Widget>[
-            //위젯을 여러개 배치하기 위함
-            Text('Hello'),
-            Text('Hello'),
-            Text('Hello'),
-          ],
+        appBar: AppBar(
+          title: Text('BBANTO'),
+          centerTitle: true, //타이틀을 가운데 정렬
+          backgroundColor: Colors.redAccent,
+          elevation: 0.0, //앱바가 뜨는 것을 없애줌
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('hello'),
+              Text('hello'),
+              Text('hello'),
+            ],
+          ),
+        ));
   }
 }
